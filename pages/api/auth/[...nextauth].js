0a87728ec10dd1699077b2eb9ddb3be787d4f334
Hannah Capell <hannah.capell@gmail.com>
2026-03-17 14:22:28 +0100

    add NextAuth config and register endpoint

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import dbConnect from "@/db/connect";
+import User from "@/db/models/User";
+import bcrypt from "bcryptjs";
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await dbConnect();
+        const user = await User.findOne({ email: credentials.email });
+        if (!user) throw new Error("No user found");
+        const isValid = await bcrypt.compare(credentials.password, user.password);
+        if (!isValid) throw new Error("Wrong password");
+        return { id: user._id, name: user.name, email: user.email };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+});
