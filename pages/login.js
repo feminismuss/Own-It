7934daaef53bef0efe2a717008b953805c849d13
Hannah Capell <hannah.capell@gmail.com>
2026-03-17 15:02:27 +0100

    add login form and login page

---
+++ pages/login.js
@@ -0,0 +1,23 @@
+import LoginForm from "@/components/LoginForm";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  async function handleLogin(data) {
+    const result = await signIn("credentials", {
+      redirect: false,
+      email: data.email,
+      password: data.password,
+    });
+
+    if (result.ok) {
+      router.push("/");
+    } else {
+      console.error("Login failed");
+    }
+  }
+
+  return <LoginForm onSubmit={handleLogin} />;
+}
