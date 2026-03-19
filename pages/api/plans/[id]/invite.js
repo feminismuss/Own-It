8c9fd91c8cfae283179fc3b983ab9ff376d34c73
Hannah Capell <hannah.capell@gmail.com>
2026-03-19 14:34:42 +0100

    add invite token endpoints

---
+++ pages/api/plans/[id]/invite.js
@@ -0,0 +1,31 @@
+import dbConnect from "@/db/connect";
+import Plan from "@/db/models/plan";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]";
+import crypto from "crypto";
+
+export default async function handler(request, response) {
+    await dbConnect();
+
+    if (request.method === "POST") {
+        try {
+            const session = await getServerSession(request, response, authOptions);
+            if (!session) {
+                return response.status(401).json({error: "Not authenticated"})
+            }
+            const {id} = request.query;
+            const plan = await Plan.findById(id);
+
+            if (!plan) return response.status(404).json({error: "Plan not found"});
+            if (plan.owner.toString() !== session.user.id){
+                return response.status(403).json({error: "Not the owner"});
+            }
+            const token = crypto.randomBytes(16).toString("hex");
+            plan.inviteToken = token;
+            await plan.save();
+            response.status(200).json({token});
+        } catch (error) {
+            response.status(500).json({error: error.message})
+        }
+    }
+}
---
+++ pages/api/plans/invite/[token].js
@@ -0,0 +1,40 @@
+import Plan from "@/db/models/plan";
+import dbConnect from "@/db/connect";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { token } = request.query;
+
+  if (request.method === "GET") {
+    try {
+      const plan = await Plan.findOne({ inviteToken: token });
+      if (!plan) return response.status(404).json({ error: "Plan not found" });
+      response.status(200).json(plan);
+      return;
+    } catch (error) {
+      response.status(500).json({ error: error.message });
+      return;
+    }
+  }
+  if (request.method === "POST") {
+    try {
+      const session = await getServerSession(request, response, authOptions);
+      if (!session) {
+        return response.status(401).json({ error: "Not authenticated" });
+      }
+      const plan = await Plan.findOne({ inviteToken: token });
+      if (!plan) return response.status(404).json({ error: "Plan not found" });
+      const allreadyMember = plan.members.includes(session.user.id);
+      if (allreadyMember) {
+        return response.status(400).json({ error: "Allready a member" });
+      }
+      plan.members.push(session.user.id);
+      await plan.save();
+      response.status(200).json({ message: "Joined successfully" });
+    } catch (error) {
+      response.status(500).json({ error: error.message });
+    }
+  }
+}
